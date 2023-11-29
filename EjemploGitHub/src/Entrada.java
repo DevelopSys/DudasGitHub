535dd44e0bd18d6a0c9a3f93ec8b0fb091b057f4
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2023-11-29 20:03:58 +0100

    Update Entrada.java

--- EjemploGitHub/src/Entrada.java
+++ EjemploGitHub/src/Entrada.java
@@ -5,6 +5,7 @@ public class Entrada {
         int[] numeros = new int[10];
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = (int) (Math.random()*98)+1;
+            // ahora quiero que los numeros no se repitan
         }
 
         for (int i = 0; i < numeros.length; i++) {
