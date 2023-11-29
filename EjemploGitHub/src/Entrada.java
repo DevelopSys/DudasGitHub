2866dbfdad9b434c0a1ffb03d68b874dc98f2b89
Develop and System <developandsystem@gmail.com>
2023-11-29 20:00:24 +0100

    bingo inicial
    rellena los numeros del carton

---
+++ EjemploGitHub/src/Entrada.java
@@ -0,0 +1,10 @@
+public class Entrada {
+
+    public static void main(String[] args) {
+        // Este es el bindo
+        int[] numeros = new int[10];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int) (Math.random()*98)+1;
+        }
+    }
+}
